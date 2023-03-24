37878cbf15f712ff9234e317e4f718e7cc61bfe5
andrejpopo <andrejpopovski@hotmail.com>
2023-03-24 08:38:53 +0100

    fixed AWS creds

--- TerraformEKS/providers.tf
+++ TerraformEKS/providers.tf
@@ -4,8 +4,6 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  shared_config_files      = ["$HOME/.aws/config"]
-  shared_credentials_files = ["$HOME/.aws/credentials"]
 }
 data "aws_availability_zones" "available" {}
 
